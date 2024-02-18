bc838889ac8c28cb452aca67556cffabceda351b
Petros Kontos <p_kontos@hotmail.com>
2024-02-18 19:54:19 +0200

    changes to entry point

--- index.ts
+++ index.ts
@@ -1,16 +1,7 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-   import { serveFile } from "https://deno.land/std/http/file_server.ts";
+import { serveDir } from "https://deno.land/std/http/file_server.ts";
 
-   const server = serve({ port: 80 });
-
-   console.log("http://localhost/");
-   for await (const request of server) {
-     const filePath = `.${request.url === "/" ? "/index.html" : request.url}`;
-     try {
-       const content = await serveFile(request, filePath);
-       request.respond(content);
-     } catch (e) {
-       console.error(e.message);
-       request.respond({ status: 404 });
-     }
-   }
+serve((req) => {
+  const url = new URL(req.url);
+  return serveDir(req, { fsRoot: "", urlRoot: "", showDirListing: true, enableCors: true });
+});
